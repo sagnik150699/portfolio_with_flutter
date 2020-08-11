2bafd47cf3f1c66c2dbab8b778a7c83871d3ba8d
Sagnik Bhattacharya <32522431+sagnik150699@users.noreply.github.com>
2020-08-11 13:40:45 +0530

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/mi_card2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mi_card2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/example/sagnik/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sagnik
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
